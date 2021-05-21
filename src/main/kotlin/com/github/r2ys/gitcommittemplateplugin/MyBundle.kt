312a520c5ac1c89d7ff2cdd0ff6bdd203c9b35bd
GitHub Action <action@github.com>
2021-05-21 07:18:26 +0000

    Template cleanup

--- src/main/kotlin/com/github/r2ys/gitcommittemplateplugin/MyBundle.kt
+++ src/main/kotlin/com/github/r2ys/gitcommittemplateplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.r2ys.gitcommittemplateplugin
 
 import com.intellij.AbstractBundle
 import org.jetbrains.annotations.NonNls
---
+++ src/main/kotlin/com/github/r2ys/gitcommittemplateplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.r2ys.gitcommittemplateplugin.services
+
+import com.github.r2ys.gitcommittemplateplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
